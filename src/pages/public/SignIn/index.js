183f13b63e03c05068c24f1b7bd653266b980680
Chaoming Li <email@chaoming.li>
2020-04-29 19:21:42 +1000

    added firebase login ui

--- src/pages/public/SignIn/index.js
+++ src/pages/public/SignIn/index.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { FirebaseAuth } from '../../../components/FirebaseAuth/firebase';
+import * as firebase from "firebase/app";
 
 const SignIn = () => {
+    const [signInSuccess, setSignInSuccess] = useState(false);
+
+    // Configure FirebaseUI.
+    const uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                setSignInSuccess(true);
+                return true;
+            }
+        },
+        signInSuccessUrl: '/',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        ]
+    };
+
     return (
-        <div>Sign In</div>
+        <>
+        {signInSuccess?(
+            <i className="fa fa-spinner fa-5x fa-spin" />
+        ):(
+        <div className="text-center">
+            <i className="fa fa-5x fa-fire text-warning"></i>
+            <h2 className="h3 mb-3 font-weight-normal">Please sign in</h2>
+            <div id="sign-in" className="SignIn">
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={FirebaseAuth.auth()} />
+            </div>
+        </div>
+        )}  
+    </>
     )
 }
 
