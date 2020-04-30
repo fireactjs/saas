fdfd8c87ecfac38d6c73844ec605aa2d2d59cec2
Chaoming Li <email@chaoming.li>
2020-04-30 21:15:59 +1000

    default app menu is constructed

--- src/components/menus/AppMenu/index.js
+++ src/components/menus/AppMenu/index.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AppMenu = () => {
     return (
         <nav className="sidebar-nav">
             <ul className="nav">
-                <li className="nav-title">Nav Title</li>
+                <li className="nav-title">Application</li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        <i className="nav-icon cui-speedometer"></i> Nav item
-                    </a>
+                    <Link className="nav-link" to="/">
+                        <i className="nav-icon fa fa-fire"></i> Dashboard
+                    </Link>
+                </li>
+                <li className="nav-title">User</li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/user/profile">
+                        <i className="nav-icon fa fa-user"></i> Profile
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/user/log">
+                        <i className="nav-icon fa fa-list"></i> Activity Log
+                    </Link>
                 </li>
             </ul>
         </nav>
