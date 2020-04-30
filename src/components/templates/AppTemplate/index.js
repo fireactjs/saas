7a085fc72f8b072311e902709d0369f5ac798c1b
Chaoming Li <email@chaoming.li>
2020-04-30 21:09:47 +1000

    hide/show sidebar

--- src/components/templates/AppTemplate/index.js
+++ src/components/templates/AppTemplate/index.js
@@ -7,7 +7,13 @@ const AppTemplate = ({ children }) => {
     return (
 		<div className="app">
             <header className="app-header navbar">
-                <button type="button" className="d-lg-none navbar-toggler" data-sidebar-toggler="true">
+                <button type="button" className="d-lg-none navbar-toggler" data-sidebar-toggler="true" onClick={() => {
+                    if(document.body.classList.contains("sidebar-show")){
+                        document.body.classList.remove("sidebar-show");
+                    }else{
+                        document.body.classList.add("sidebar-show");
+                    }
+                }}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <NavLink className="navbar-brand" to="/">
@@ -16,7 +22,13 @@ const AppTemplate = ({ children }) => {
                     </div>
                     <div className="navbar-brand-full text-dark pl-2 font-weight-bold">FIREACT</div>
                 </NavLink>
-                <button type="button" className="d-md-down-none navbar-toggler" data-sidebar-toggler="true">
+                <button type="button" className="d-md-down-none navbar-toggler" data-sidebar-toggler="true"  data-toggle="sidebar-show" onClick={() => {
+                    if(document.body.classList.contains("sidebar-minimized")){
+                        document.body.classList.remove("sidebar-minimized");
+                    }else{
+                        document.body.classList.add("sidebar-minimized");
+                    }
+                }}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <ul className="ml-auto navbar-nav">
@@ -26,15 +38,18 @@ const AppTemplate = ({ children }) => {
             <div className="app-body">
                 <div className="sidebar">
                     <AppMenu />
-                    <button className="sidebar-minimizer brand-minimizer" type="button" />
+                    <button className="sidebar-minimizer brand-minimizer" type="button" onClick={(e) => {
+                        if(document.body.classList.contains("sidebar-minimized")){
+                            document.body.classList.remove("sidebar-minimized");
+                        }else{
+                            document.body.classList.add("sidebar-minimized");
+                        }
+                    }} />
                 </div>
                 <main className="main">
                 {children}
                 </main>
             </div>
-            <footer className="app-footer">
-
-            </footer>
         </div>
     )
 }
