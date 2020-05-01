99798f5b250baf02fb3ce8b41f367b459b9c1158
Chaoming Li <email@chaoming.li>
2020-05-01 13:30:45 +1000

    set active SidebarLink based on pathname

--- src/components/menus/AppMenu/index.js
+++ src/components/menus/AppMenu/index.js
@@ -7,18 +7,18 @@ const AppMenu = () => {
             <ul className="nav">
                 <li className="nav-title">Application</li>
                 <li className="nav-item">
-                    <SidebarLink className="nav-link active" to="/">
+                    <SidebarLink className={"nav-link"+(window.location.pathname==='/'?" active":"")} to="/">
                         <i className="nav-icon fa fa-fire"></i> Dashboard
                     </SidebarLink>
                 </li>
                 <li className="nav-title">User</li>
                 <li className="nav-item">
-                    <SidebarLink className="nav-link" to="/user/profile">
+                    <SidebarLink className={"nav-link"+(window.location.pathname.startsWith('/user/profile')?" active":"")} to="/user/profile">
                         <i className="nav-icon fa fa-user"></i> Profile
                     </SidebarLink>
                 </li>
                 <li className="nav-item">
-                    <SidebarLink className="nav-link" to="/user/log">
+                    <SidebarLink className={"nav-link"+(window.location.pathname.startsWith('/user/log')?" active":"")} to="/user/log">
                         <i className="nav-icon fa fa-list"></i> Activity Log
                     </SidebarLink>
                 </li>
