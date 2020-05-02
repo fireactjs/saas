9f61431f99825ded6eef3fdbce8551672b5fae1a
Chaoming Li <email@chaoming.li>
2020-05-02 20:35:09 +1000

    bug fixes for Alert

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -3,10 +3,10 @@ import React from "react";
 const Alert = ({type, message, dismissible}) => {
 
     return (
-        <div class={"alert fade show"+" alert-"+type+(dismissible?" alert-dismissible":"")} role="alert">
+        <div className={"alert fade show alert-"+type+(dismissible?" alert-dismissible":"")} role="alert">
             {message}
             {dismissible && 
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close" >
+                <button type="button" className="close" data-dismiss="alert" aria-label="Close" >
                     <span aria-hidden="true">&times;</span>
                 </button>
             }
