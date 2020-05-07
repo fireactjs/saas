6c8916a6328f22628f8eaf78ab69796347c609f6
Chaoming Li <email@chaoming.li>
2020-05-07 15:22:32 +1000

    updated Users collection and activity logs for sign in.

---
+++ src/components/log/index.js
@@ -0,0 +1,29 @@
+import { FirebaseAuth } from "../FirebaseAuth/firebase";
+import * as firebase from "firebase/app";
+
+export const log = action => {
+    const Firestore = FirebaseAuth.firestore();
+    const currentUser = FirebaseAuth.auth().currentUser;
+
+    const dt = new Date();
+    const data ={
+        'action': action,
+        'time': dt
+    }
+    const userDocRef = Firestore.collection('users').doc(currentUser.uid);
+    userDocRef.collection('activities').doc(''+dt.getTime()).set(data).then(() => {
+        userDocRef.set({'activityCount':firebase.firestore.FieldValue.increment(1)},{merge: true}).then(() => {
+
+        }).catch(err => {
+
+        });
+    }).catch(err => {
+
+    });
+}
+export const SIGN_IN = 'signed in';
+export const UPDATE_PASSWORD = 'changed password';
+export const UPDATE_USERNAME = 'changed user name';
+export const UPDATE_PHONE = 'changed phone number';
+export const UPDATE_EMAIL = 'changed email address';
+export const SIGN_OUT = 'signed out';
--- src/pages/public/SignIn/index.js
+++ src/pages/public/SignIn/index.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { FirebaseAuth } from '../../../components/FirebaseAuth/firebase';
 import * as firebase from "firebase/app";
+import { userSignIn } from '../../../components/user/functions';
 
 const SignIn = () => {
-    const [signInSuccess, setSignInSuccess] = useState(false);
+
+    const [signInSuccess, setSignInSuccess] = useState(null);
 
     // Configure FirebaseUI.
     const uiConfig = {
         callbacks: {
             signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-                setSignInSuccess(true);
-                return true;
+                userSignIn((result) => {
+                    if(result){
+                        setSignInSuccess(true);
+                        window.location = '/';
+                    }else{
+                        setSignInSuccess(false);
+                    }
+                })
+                return false;
             }
         },
         signInSuccessUrl: '/',
@@ -25,18 +35,27 @@ const SignIn = () => {
 
     return (
         <>
-        {signInSuccess?(
+        {signInSuccess &&
             <i className="fa fa-spinner fa-5x fa-spin" />
-        ):(
-        <div className="text-center">
-            <i className="fa fa-5x fa-fire text-warning"></i>
-            <h2 className="h3 mb-3 font-weight-normal">Please sign in</h2>
-            <div id="sign-in" className="SignIn">
-                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={FirebaseAuth.auth()} />
+        }
+        {signInSuccess === null &&
+            <div className="text-center">
+                <i className="fa fa-5x fa-fire text-warning"></i>
+                <h2 className="h3 mb-3 font-weight-normal">Please sign in</h2>
+                <div id="sign-in" className="SignIn">
+                    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={FirebaseAuth.auth()} />
+                </div>
             </div>
-        </div>
-        )}  
-    </>
+        }
+        {signInSuccess === false &&
+            <div className="text-center">
+                <i className="fa fa-5x fa-fire text-warning"></i>
+                <h1>Server Error</h1>
+                <p>Oops, something went wrong, please try again.</p>
+                <Link to="/">Home</Link>
+            </div>
+        }
+        </>
     )
 }
 
