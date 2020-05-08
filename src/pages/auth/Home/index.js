b2663394cb6bcf6c92416c6c527b23ec7c5ac08d
Chaoming Li <email@chaoming.li>
2020-05-08 20:44:55 +1000

    updated Home with instructions

--- src/pages/auth/Home/index.js
+++ src/pages/auth/Home/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Breadcrumb from '../../../components/Breadcrumb';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const title = 'Dashboard';
+
     const breadcrumbLinks = [
         {
             to: "/",
@@ -10,7 +13,7 @@ const Home = () => {
         },
         {
             to: null,
-            text: "Dashboard",
+            text: title,
             active: true
         }
     ];
@@ -20,7 +23,18 @@ const Home = () => {
             <Breadcrumb links={breadcrumbLinks} />
             <div className="container-fluid">
                 <div className="animated fadeIn">
-                    Home
+                    <div className="card">
+                        <div className="card-header">
+                            {title}
+                        </div>
+                        <div className="card-body">
+                            <ul>
+                                <li>Modify <code>/src/pages/auth/Home/index.js</code> to change this page.</li>
+                                <li><Link to="/user/profile">User Profile Features</Link></li>
+                                <li><Link to="/user/log">Your Activity Logs</Link></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
