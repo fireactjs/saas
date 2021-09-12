1d10f358e97b25485b33049e0c3410a8e43713bf
Chaoming Li <email@chaoming.li>
2021-09-12 23:41:16 +1000

    added AppBar and Drawer

--- src/components/templates/AppTemplate/index.js
+++ src/components/templates/AppTemplate/index.js
@@ -3,13 +3,50 @@ import UserMenu from '../../menus/UserMenu';
 import AppMenu from '../../menus/AppMenu';
 import Logo from '../../Logo';
 import {BreadcrumbContext, Breadcrumb} from '../../Breadcrumb';
+import { CssBaseline, AppBar, Toolbar, IconButton, Typography, Drawer, Divider, List, ListItem, ListItemText } from '@material-ui/core';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 const AppTemplate = ({ children }) => {
 
     const [breadcrumb, setBreadcrumb] = useState([]);
 
+    const [open, setOpen] = useState(true);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    }
+    const handleDrawerClose = () => {
+        setOpen(false);
+    }
+
     return (
+        <>
+            <CssBaseline />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon onClick={handleDrawerOpen} />
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer open={open} variant="persistent" anchor="left" >
+                <IconButton onClick={handleDrawerClose}>
+                    <ChevronLeftIcon />
+                </IconButton>
+                <Divider />
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        
 		<div className="c-app">
             <div className="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
                 <div className="c-sidebar-brand d-md-down-none">
@@ -44,6 +81,7 @@ const AppTemplate = ({ children }) => {
             </div>
             
         </div>
+        </>
     )
 }
 
