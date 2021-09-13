68525b158ba727246a8736f215e6032a4d436f04
Chaoming Li <email@chaoming.li>
2021-09-13 20:58:37 +1000

    fxied warning about key in Breadcrumb

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -8,16 +8,16 @@ export const Breadcrumb = ({links}) => {
     return (
         <Breadcrumbs>
             {links !== null && links.map((link, key) => {
-                    return (
-                        <>
-                            {link.to !== null && 
-                                <Link to={link.to} component={RouterLink}>{link.text}</Link>
-                            }
-                            {link.to === null && 
-                                <Typography color="textPrimary">{link.text}</Typography>
-                            }
-                        </>
-                    )
+                    if(link.to !== null){
+                        return (
+                            <Link key={key} to={link.to} component={RouterLink}>{link.text}</Link>  
+                        )
+                    }else{
+                        return (
+                            <Typography key={key} color="textPrimary">{link.text}</Typography>
+                        )
+                    }
+                    
                 })
             }
         </Breadcrumbs>
