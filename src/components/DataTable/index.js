801deb69ae92ca57ab17f811b8a55aa178c1761b
Chaoming Li <email@chaoming.li>
2021-09-14 23:37:59 +1000

    updated ViewLogs with DataTable

---
+++ src/components/DataTable/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, TablePagination } from '@material-ui/core';
+
+const DataTable = ({columns, rows, totalRows, pageSize, page, handlePageChane, handlePageSizeChange}) => {
+
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                        {columns.map((c,i) => 
+                            <TableCell key={i}>{c.name}</TableCell>
+                        )}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((r, i) =>
+                        <TableRow key={i}>
+                        {columns.map((c, k) =>
+                            <TableCell key={k}>{r[c.field]}</TableCell>
+                        )}
+                        </TableRow>
+                    )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10,20,50,100]}
+                component="div"
+                count={totalRows}
+                rowsPerPage={pageSize}
+                page={page}
+                onPageChange={handlePageChane}
+                onRowsPerPageChange={handlePageSizeChange}
+            />
+        </>
+    )
+}
+
+export default DataTable;
--- src/pages/auth/user/ViewLogs/index.js
+++ src/pages/auth/user/ViewLogs/index.js
@@ -1,41 +1,43 @@
-import { Button } from "@material-ui/core";
 import React, { useState, useEffect, useContext, useRef } from "react";
 import { BreadcrumbContext } from '../../../../components/Breadcrumb';
 import { FirebaseAuth } from "../../../../components/FirebaseAuth/firebase";
 import Loader from '../../../../components/Loader';
 import UserPageLayout from '../../../../components/user/UserPageLayout';
-import { Paper, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, } from '@material-ui/core';
-const ViewLogs = () => {
-    const pageSize = 10;
+import DataTable from "../../../../components/DataTable";
 
+const ViewLogs = () => {
     const [total, setTotal] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
+    const [page, setPage] = useState(0);
+    const [rows, setRows] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [qs, setQs] = useState(null);
+    const mountedRef = useRef(true);
+
     const getTotal = () => {
+        setLoading(true);
         const userDocRef = FirebaseAuth.firestore().collection('users').doc(FirebaseAuth.auth().currentUser.uid);
         userDocRef.get().then(function(userDoc){
             if (!mountedRef.current) return null
             if(userDoc.exists){
                 setTotal(userDoc.data().activityCount);
+                setLoading(false);
             }
         })
     }
 
-    // document snapshots
-    const [qs, setQs] = useState(null);
-    const mountedRef = useRef(true);
-
-    const [rows, setRows] = useState([]);
-    const [count, setCount] = useState(0);
-    const [loading, setLoading] = useState(false);
-
-    const getLogs = (pageSize, lastDoc) => {
+    const getLogs = (pz, direction, doc) => {
         setLoading(true);
         let records = [];
         const collectionRef = FirebaseAuth.firestore().collection('users').doc(FirebaseAuth.auth().currentUser.uid).collection('activities');
         let query = collectionRef.orderBy('time', 'desc');
-        if(lastDoc){
-            query = query.startAfter(lastDoc);
+        if(direction && direction === 'next'){
+            query = query.startAfter(doc);
         }
-        query = query.limit(pageSize);
+        if(direction && direction === 'previous'){
+            query = query.endBefore(doc);
+        }
+        query = query.limit(pz);
         query.get().then(documentSnapshots => {
             if (!mountedRef.current) return null
             documentSnapshots.forEach(doc => {
@@ -46,11 +48,12 @@ const ViewLogs = () => {
                 });
             });
             if(records.length > 0){
-                setRows(rows => rows.concat(records));
+                setRows(records);
                 setQs(documentSnapshots);
-                setCount(count => documentSnapshots.size+count);
             }
             setLoading(false);
+        }).catch(e => {
+            console.log(e);
         });
     }
 
@@ -76,55 +79,50 @@ const ViewLogs = () => {
             }
         ]);
         getTotal();
-        getLogs(pageSize);
         return () => { 
             mountedRef.current = false
         }
     },[setBreadcrumb]);
 
+    useEffect(() => {
+        getLogs(pageSize);
+    },[pageSize]);
+
     return (
         <UserPageLayout>
-            {total > 0 ? (
-                <>
-                    <TableContainer component={Paper}>
-                        <Table aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Activity</TableCell>
-                                    <TableCell>Time</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {rows.map((r,i) => 
-                                <TableRow key={i}>
-                                    <TableCell component="th" scope="row">
-                                        {r.action}
-                                    </TableCell>
-                                    <TableCell>
-                                        {r.time}
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <p></p>
-                    <Button variant="contained" color="primary" disabled={(total===count || loading)} onClick={(e) => {
-                        e.preventDefault();
-                        getLogs(pageSize, qs.docs[qs.docs.length-1]);
-                    }} >
-                        {loading && <Loader />} More activities...
-                    </Button>
-                </>
+            {loading?(
+                <Loader text="Loading logs..."></Loader>
             ):(
                 <>
-                {(qs === null) ? (
-                    <Loader text="loading logs..."></Loader>
+                {total > 0 ? (
+                    <DataTable columns={[
+                        {name: "Activity", field: "action"},
+                        {name: "Time", field: "time"}
+                    ]}
+                    rows={rows}
+                    totalRows={total}
+                    pageSize={pageSize}
+                    page={page}
+                    handlePageChane={(e, p) => {
+                        if(p>page){
+                            getLogs(pageSize, 'next', qs.docs[qs.docs.length-1]);
+                        }
+                        if(p<page){
+                            getLogs(pageSize, 'previous', qs.docs[0]);
+                        }
+                        setPage(p);
+                    }}
+                    handlePageSizeChange={(e) => {
+                        setPageSize(e.target.value);
+                        setPage(0);
+                    }}
+                    ></DataTable>
                 ):(
                     <div>No activity is found</div>
                 )}
                 </>
             )}
+            
         </UserPageLayout>
         
     )
