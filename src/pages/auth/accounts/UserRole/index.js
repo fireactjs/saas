d55a15d7aef91a1fd74f919e690a80856ec2c36d
Chaoming Li <email@chaoming.li>
2021-09-19 14:56:10 +1000

    updated UserRole

--- src/pages/auth/accounts/UserRole/index.js
+++ src/pages/auth/accounts/UserRole/index.js
@@ -1,15 +1,15 @@
 import React, { useContext, useEffect, useState, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { BreadcrumbContext } from '../../../../components/Breadcrumb';
 import { AuthContext } from "../../../../components/FirebaseAuth";
 import { CloudFunctions } from "../../../../components/FirebaseAuth/firebase";
 import Loader from "../../../../components/Loader";
-import UserAvatar from '../../../../components/UserAvatar';
-import Alert from "../../../../components/Alert";
-import {Form, Field} from "../../../../components/Form";
+import {Form} from "../../../../components/Form";
+import { Paper, Box, Alert, Select, MenuItem, FormControl, InputLabel, Stack, Button, Typography, Avatar } from "@mui/material";
 
 const UserRole = () => {
     const title = 'Change User Role';
+    const history = useHistory();
 
     const { userData } = useContext(AuthContext);
     const { userId } = useParams();
@@ -67,78 +67,80 @@ const UserRole = () => {
 
     return (
         <>
-            <div className="container-fluid">
-                <div className="animated fadeIn">
-                    <div className="card">
-                        <div className="card-header">
-                            {title}
-                        </div>
-                        <div className="card-body">
-                            {(userId !== userData.currentAccount.owner)?(
-                                <>
-                                    {success && 
-                                        <Alert type="success" message="User role is successfully updated." dismissible={true} onDismiss={() => setSuccess(false)}></Alert>
-                                    }
-                                    {error !== null && 
-                                        <Alert type="danger" message={error} dismissible={true} onDismiss={() => setError(null)}></Alert>
-                                    }
-                                    {user === null ? (
-                                        <Loader text="Loading user details" />
-                                    ):(
-                                        <Form handleSubmit={e => {
-                                            e.preventDefault();
-                                            setError(null);
-                                            setSuccess(false);
-                                            setInSubmit(true);
-                                            const updateAccountUserRole = CloudFunctions.httpsCallable('updateAccountUserRole');
-                                            updateAccountUserRole({
-                                                accountId: userData.currentAccount.id,
-                                                userId: userId,
-                                                role: selectedRole
-                                            }).then(res => {
-                                                setInSubmit(false);
-                                                setSuccess(true);
-                                            }).catch(err => {
-                                                setInSubmit(false);
-                                                setError(err.message);
-                                            });
-                                        }}
-                                        disabled={selectedRole===null || inSubmit}
-                                        submitBtnStyle={(selectedRole!=='remove')?"primary":"danger"}
-                                        inSubmit={inSubmit}
-                                        enableDefaultButtons={true}
-                                        backToUrl={"/account/"+userData.currentAccount.id+"/users"}
-                                        >
-                                            <Field label="User Name">
-                                                <div className="row col">
-                                                    <UserAvatar name={user.displayName} photoUrl={user.photoUrl} className="c-avatar-img mr-2"></UserAvatar>
-                                                    <div className="pt-2 ml-1">{user.displayName}</div>
-                                                </div>
-                                            </Field>
-                                            <Field label="Last Login Time">
-                                                <div className="my-1">{user.lastLoginTime.toLocaleString()}</div>
-                                            </Field>
-                                            <Field label="Role">
-                                                <select className="form-control col-md-6 col-sm-8" defaultValue={user.role} onChange={e => {
-                                                    setSelectedRole(e.target.value);
-                                                }}>
-                                                    <option value="user">user</option>
-                                                    <option value="admin">admin</option>
-                                                    <option value="remove">-- Remove Access --</option>
-                                                </select>
-                                            </Field>
-                                        </Form>
-                                    )}
-                                </>
+            <Paper>
+                <Box p={2}>
+                {(userId !== userData.currentAccount.owner)?(
+                    success?(
+                        <>
+                            <Alert severity="success" onClose={() => setSuccess(false)}>User role is successfully updated.</Alert>
+                            <Stack direction="row" spacing={1} mt={2}>
+                                <Button variant="contained" color="primary" onClick={() => history.push("/account/"+userData.currentAccount.id+"/users")} >Back to User List</Button>
+                            </Stack>
+                        </>
+
+                    ):(
+                        <Stack spacing={3}>
+                            {error !== null && 
+                                <Alert severity="error">{error}</Alert>
+                            }
+                            {user === null ? (
+                                <Loader text="Loading user details" />
                             ):(
-                                <Alert type="danger" message="Access Denied." dismissible={false} ></Alert>
+                                <Form handleSubmit={e => {
+                                    e.preventDefault();
+                                    setError(null);
+                                    setSuccess(false);
+                                    setInSubmit(true);
+                                    const updateAccountUserRole = CloudFunctions.httpsCallable('updateAccountUserRole');
+                                    updateAccountUserRole({
+                                        accountId: userData.currentAccount.id,
+                                        userId: userId,
+                                        role: selectedRole
+                                    }).then(res => {
+                                        setInSubmit(false);
+                                        setSuccess(true);
+                                    }).catch(err => {
+                                        setInSubmit(false);
+                                        setError(err.message);
+                                    });
+                                }}
+                                disabled={selectedRole===null || selectedRole===user.role || inSubmit}
+                                submitBtnStyle={(selectedRole!=='remove')?"primary":"error"}
+                                inSubmit={inSubmit}
+                                enableDefaultButtons={true}
+                                backToUrl={"/account/"+userData.currentAccount.id+"/users"}
+                                >
+                                    <Stack spacing={1} mb={5} style={{display: 'inline-block', textAlign: 'center'}}>
+                                        <Avatar alt={user.displayName} src={user.photoUrl} sx={{width: 100, height:100, margin: 'auto'}} />
+                                        <Typography><strong style={{fontSize: '1.3rem'}}>{user.displayName}</strong></Typography>
+                                        <Typography>
+                                            Last Login:<br /> {user.lastLoginTime.toLocaleString()}
+                                        </Typography>
+                                    </Stack>
+                                    <FormControl fullWidth>
+                                        <InputLabel>Role</InputLabel>
+                                        <Select label="Role" defaultValue={user.role} onChange={e => {
+                                            setSelectedRole(e.target.value);
+                                        }}>
+                                            <MenuItem value="user">user</MenuItem>
+                                            <MenuItem value="admin">admin</MenuItem>
+                                            <MenuItem value="remove"><em>-- Remove Access --</em></MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </Form>
                             )}
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </Stack>
+                    )
+                ):(
+                    <>
+                        <Alert severity="error">Cannot change account owner role.</Alert>
+                        <Stack direction="row" spacing={1} mt={2}>
+                            <Button variant="contained" color="primary" onClick={() => history.push("/account/"+userData.currentAccount.id+"/users")} >Back to User List</Button>
+                        </Stack>
+                    </>
+                )}
+                </Box>
+            </Paper>
         </>
 
     )
