174d642f0cce15ee532e87c5a37040938426f23d
Chaoming Li <email@chaoming.li>
2021-09-19 21:55:36 +1000

    removed components not in use

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,10 @@
 import Form from './Form';
-import Field from './Field';
 import Input from './Input';
 import ButtonRow from './ButtonRow';
 import FormResult from './FormResult';
 
 export {
     Form,
-    Field,
     Input,
     ButtonRow,
     FormResult,
