bb96ff1d0c81e10aaf7493377e2332f40c9428a3
Chaoming Li <email@chaoming.li>
2021-09-19 15:31:34 +1000

    updated Invite

--- src/pages/auth/user/Invite/index.js
+++ src/pages/auth/user/Invite/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Redirect, useParams } from "react-router-dom";
+import React, { useState, useContext, useEffect, useRef } from "react";
+import { Redirect, useParams, useHistory } from "react-router-dom";
 import { CloudFunctions } from "../../../../components/FirebaseAuth/firebase";
 import { BreadcrumbContext } from '../../../../components/Breadcrumb';
 import Loader from "../../../../components/Loader";
-import Alert from "../../../../components/Alert";
 import { Link } from "react-router-dom";
+import { Paper, Box, Stack, Alert, Typography, Button } from "@mui/material";
 
 
 const Invite = () => {
@@ -12,6 +12,8 @@ const Invite = () => {
     const { code } = useParams();
 
     const title = 'View Invite';
+    const history = useHistory();
+    const mountedRef = useRef(true);
 
     const [invite, setInvite] = useState(null); 
     const [error, setError] = useState(null);
@@ -39,66 +41,66 @@ const Invite = () => {
             getInvite({
                 inviteId: code
             }).then(res => {
+                if (!mountedRef.current) return null
                 if(isSubscribed){
                     setInvite(res.data);
                 }
             }).catch(err => {
+                if (!mountedRef.current) return null
                 if(isSubscribed){
                     setError(err.message);
                 }
             });
             return () => (isSubscribed = false);
         }
-    }, [code, setBreadcrumb, title])
+    }, [code, setBreadcrumb, title]);
+
+    useEffect(() => {
+        return () => { 
+            mountedRef.current = false
+        }
+    },[]);
 
     return (
         <>
             {success?(
                 <Redirect to={"/account/"+invite.accountId+"/"}></Redirect>
             ):(
-                <>
-                    <div className="container-fluid">
-                        <div className="animated fadeIn">
-                            <div className="card">
-                                <div className="card-header">
-                                    {title}
-                                </div>
-                                <div className="card-body">
-                                    {error !== null && 
-                                        <Alert type="danger" message={error} dismissible={true} onDismiss={() => setError(null)}></Alert>
-                                    }
-                                    {invite === null?(
-                                        <Loader text="Loading the invite..."></Loader>
-                                    ):(
-                                        <>
-                                            <div className="text-center">This invite will grant you access to <strong>{invite.accountName}</strong>. Do you want to accept it?</div>
-                                            <div className="text-center mt-3">
-                                                <button className="btn btn-primary mr-2" disabled={inSubmit} onClick={e => {
-                                                    e.preventDefault();
-                                                    setInSubmit(true);
-                                                    const acceptInvite = CloudFunctions.httpsCallable('acceptInvite');
-                                                    acceptInvite({
-                                                        inviteId: code
-                                                    }).then(res => {
-                                                        setSuccess(true);
-                                                    }).catch(err => {
-                                                        setError(err.message);
-                                                    });
-                                                }}>{inSubmit && <Loader />}
-                                                    Yes, accept the invite</button>
-                                                <Link className={inSubmit?("btn btn-secondary ml-2 btn-disabled"):("btn btn-secondary ml-2")} to={"/"} onClick={e => {
-                                                    if(inSubmit){
-                                                        e.preventDefault();
-                                                    }
-                                                }}>Ignore</Link>
-                                            </div>
-                                        </>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>
+                <Paper>
+                    <Box p={2}>
+                        <Stack spacing={3}>
+                            {error !== null && 
+                                <Alert severity="danger">{error}</Alert>
+                            }
+                            {invite === null?(
+                                <Loader text="Loading the invite..."></Loader>
+                            ):(
+                                <>
+                                    <Typography>This invite will grant you access to <strong>{invite.accountName}</strong>. Do you want to accept it?</Typography>
+                                    <Stack direction="row" spacing={1} mt={2}>
+                                        <Button disabled={inSubmit} variant="contained" onClick={e => {
+                                            e.preventDefault();
+                                            setInSubmit(true);
+                                            const acceptInvite = CloudFunctions.httpsCallable('acceptInvite');
+                                            acceptInvite({
+                                                inviteId: code
+                                            }).then(res => {
+                                                if (!mountedRef.current) return null
+                                                setSuccess(true);
+                                            }).catch(err => {
+                                                if (!mountedRef.current) return null
+                                                setError(err.message);
+                                            });
+                                        }}>{inSubmit && <Loader />}
+                                        Yes, accept the invite
+                                        </Button>
+                                        <Button color="secondary" variant="contained" disabled={inSubmit} onClick={() => history.push('/')}>Ignore</Button>
+                                    </Stack>
+                                </>
+                            )}
+                        </Stack>
+                    </Box>
+                </Paper>
             )}
         </>
     )
