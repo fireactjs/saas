5bb897308ee98b9534cd4f5bab5a074a94dafc9d
Chaoming Li <email@chaoming.li>
2022-11-20 12:23:45 +1100

    added Plans

---
+++ src/lib/components/Plans.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Plans = () => {
+    return (
+        <div>
+            select a plan
+        </div>
+    )
+}
---
+++ src/lib/components/index.js
@@ -0,0 +1,5 @@
+import { Plans } from "./Plans";
+
+export {
+    Plans
+}
