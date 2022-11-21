0d95d4207a6d8a96c51a082146608dd90d1a33a8
Chaoming Li <email@chaoming.li>
2022-11-21 22:19:47 +1100

    updated pricing json

--- src/lib/components/Plans.js
+++ src/lib/components/Plans.js
@@ -1,115 +1,129 @@
 import React from "react";
-import { Container, Grid, Card, CardHeader, CardContent, Typography, CardActions, Button, Box } from '@mui/material';
+import { Container, Paper, Grid, Card, CardHeader, CardContent, Typography, CardActions, Button, Box } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
+import { SetPageTitle } from "@fireactjs/core";
 
 export const Plans = () => {
 
     const plans = [
         {
             title: 'Free',
+            popular: false,
             price: 0,
             description: [
                 '10 users included',
                 '2 GB of storage',
                 'Help center access',
                 'Email support',
-            ],
-            buttonText: 'Sign up for free',
-            buttonVariant: 'outlined',
+            ]
         },
         {
             title: 'Pro',
-            subheader: 'Most popular',
+            popular: true,
             price: 15,
             description: [
                 '20 users included',
                 '10 GB of storage',
                 'Help center access',
                 'Priority email support',
-            ],
-            buttonText: 'Get started',
-            buttonVariant: 'contained',
+            ]
         },
         {
             title: 'Enterprise',
+            popular: false,
             price: 30,
             description: [
                 '50 users included',
                 '30 GB of storage',
                 'Help center access',
                 'Phone & email support',
-            ],
-            buttonText: 'Contact us',
-            buttonVariant: 'outlined',
+            ]
         },
     ];
 
     return (
-<Container maxWidth="md" component="main">
-    <Grid container spacing={5} alignItems="flex-end">
-    {plans.map((tier) => (
-        // Enterprise card is full width at sm breakpoint
-        <Grid
-        item
-        key={tier.title}
-        xs={12}
-        sm={tier.title === 'Enterprise' ? 12 : 6}
-        md={4}
-        >
-            <Card>
-                <CardHeader
-                title={tier.title}
-                subheader={tier.subheader}
-                titleTypographyProps={{ align: 'center' }}
-                action={tier.title === 'Pro' ? <StarIcon color="success" /> : null}
-                subheaderTypographyProps={{
-                align: 'center',
-                }}
-                sx={{
-                backgroundColor: (theme) =>
-                theme.palette.mode === 'light'
-                ? theme.palette.grey[200]
-                : theme.palette.grey[700],
-                }}
-                />
-                <CardContent>
-                    <Box
-                    sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'baseline',
-                    mb: 2,
-                    }}
+        <Container maxWidth="lg">
+            <SetPageTitle title="Choose Plan" />
+            <Paper>
+                <Box p={5}>
+                    <Typography
+                    component="h1"
+                    variant="h2"
+                    align="center"
+                    color="text.primary"
+                    gutterBottom
                     >
-                        <Typography component="h2" variant="h3" color="text.primary">
-                        ${tier.price}
-                        </Typography>
-                        <Typography variant="h6" color="text.secondary">
-                        /mo
-                        </Typography>
-                    </Box>
-                    <ul style={{listStyleType: 'none', paddingLeft: '0px'}}>
-                    {tier.description.map((line) => (
-                        <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
+                        Pricing
+                    </Typography>
+                    <Typography variant="h5" align="center" color="text.secondary" component="p" mb={5}>
+                        Choose a plan that suits you to get started.
+                    </Typography>
+                    <Grid container spacing={5} alignItems="flex-end">
+                    {plans.map((plan) => (
+                        // Enterprise card is full width at sm breakpoint
+                        <Grid
+                        item
+                        key={plan.title}
+                        xs={12}
+                        sm={plan.title === 'Enterprise' ? 12 : 6}
+                        md={4}
                         >
-                            {line}
-                        </Typography>
+                            <Card>
+                                <CardHeader
+                                title={plan.title}
+                                subheader={plan.popular ? "Most Popular": ""}
+                                titleTypographyProps={{ align: 'center' }}
+                                action={plan.popular ? <StarIcon color="success" /> : null}
+                                subheaderTypographyProps={{
+                                align: 'center',
+                                }}
+                                sx={{
+                                backgroundColor: (theme) =>
+                                theme.palette.mode === 'light'
+                                ? theme.palette.grey[200]
+                                : theme.palette.grey[700],
+                                }}
+                                />
+                                <CardContent>
+                                    <Box
+                                    sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'baseline',
+                                    mb: 2,
+                                    }}
+                                    >
+                                        <Typography component="h2" variant="h3" color="text.primary">
+                                        ${plan.price}
+                                        </Typography>
+                                        <Typography variant="h6" color="text.secondary">
+                                        /mo
+                                        </Typography>
+                                    </Box>
+                                    <ul style={{listStyleType: 'none', paddingLeft: '0px'}}>
+                                    {plan.description.map((line) => (
+                                        <Typography
+                                        component="li"
+                                        variant="subtitle1"
+                                        align="center"
+                                        key={line}
+                                        >
+                                            {line}
+                                        </Typography>
+                                    ))}
+                                    </ul>
+                                </CardContent>
+                                <CardActions>
+                                    <Button fullWidth variant={plan.popular?"contained":"outlined"}>
+                                    Get Started
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
                     ))}
-                    </ul>
-                </CardContent>
-                <CardActions>
-                    <Button fullWidth variant={tier.buttonVariant}>
-                    {tier.buttonText}
-                    </Button>
-                </CardActions>
-            </Card>
-        </Grid>
-    ))}
-    </Grid>
-</Container>
+                    </Grid>
+                </Box>
+            </Paper>
+        </Container>
     )
 }
