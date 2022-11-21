73d402ed6e5aa24d596e674f167cc3b87ac40d18
Chaoming Li <chaoming@users.noreply.github.com>
2022-11-22 08:42:34 +1100

    create a price table component

--- src/lib/components/Plans.js
+++ src/lib/components/Plans.js
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Paper, Grid, Card, CardHeader, CardContent, Typography, CardActions, Button, Box } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import { SetPageTitle } from "@fireactjs/core";
 
+const PriceTable = ({setPriceId, plans}) => {
+
+    return (
+        <Box p={5}>
+        <Typography
+        component="h1"
+        variant="h3"
+        align="center"
+        color="text.primary"
+        gutterBottom
+        mb={8}
+        >
+            Choose Your Plan
+            </Typography>
+            <Grid container spacing={5} alignItems="flex-end">
+            {plans.map((plan) => (
+                // Enterprise card is full width at sm breakpoint
+                <Grid
+                item
+                key={plan.title}
+                xs={12}
+                sm={plan.title === 'Enterprise' ? 12 : 6}
+                md={4}
+                >
+                    <Card>
+                        <CardHeader
+                        title={plan.title}
+                        subheader={plan.popular ? "Most Popular": ""}
+                        titleTypographyProps={{ align: 'center' }}
+                        action={plan.popular ? <StarIcon color="success" /> : null}
+                        subheaderTypographyProps={{
+                        align: 'center',
+                        }}
+                        sx={{
+                        backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                        }}
+                        />
+                        <CardContent>
+                            <Box
+                            sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'baseline',
+                            mb: 2,
+                            }}
+                            >
+                                <Typography variant="h4" color="text.primary">
+                                {plan.currency}
+                                </Typography>
+                                <Typography component="h2" variant="h3" color="text.primary">
+                                {plan.price}
+                                </Typography>
+                                <Typography variant="h6" color="text.secondary">
+                                /{plan.frequency}
+                                </Typography>
+                            </Box>
+                            <ul style={{listStyleType: 'none', paddingLeft: '0px'}}>
+                            {plan.description.map((line) => (
+                                <Typography
+                                component="li"
+                                variant="subtitle1"
+                                align="center"
+                                key={line}
+                                >
+                                    {line}
+                                </Typography>
+                            ))}
+                            </ul>
+                        </CardContent>
+                        <CardActions>
+                            <Button fullWidth variant={plan.popular?"contained":"outlined"} onClick={() => setPriceId(plan.priceId)}>
+                            Continue
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            ))}
+            </Grid>
+        </Box>
+    )
+}
+
+
 export const Plans = () => {
+
+    const [priceId, setPriceId] = useState("");
 
     const plans = [
         {
@@ -54,85 +142,9 @@ export const Plans = () => {
         <Container maxWidth="lg">
             <SetPageTitle title="Choose Plan" />
             <Paper>
-                <Box p={5}>
-                    <Typography
-                    component="h1"
-                    variant="h3"
-                    align="center"
-                    color="text.primary"
-                    gutterBottom
-                    mb={8}
-                    >
-                        Choose Your Plan
-                    </Typography>
-                    <Grid container spacing={5} alignItems="flex-end">
-                    {plans.map((plan) => (
-                        // Enterprise card is full width at sm breakpoint
-                        <Grid
-                        item
-                        key={plan.title}
-                        xs={12}
-                        sm={plan.title === 'Enterprise' ? 12 : 6}
-                        md={4}
-                        >
-                            <Card>
-                                <CardHeader
-                                title={plan.title}
-                                subheader={plan.popular ? "Most Popular": ""}
-                                titleTypographyProps={{ align: 'center' }}
-                                action={plan.popular ? <StarIcon color="success" /> : null}
-                                subheaderTypographyProps={{
-                                align: 'center',
-                                }}
-                                sx={{
-                                backgroundColor: (theme) =>
-                                theme.palette.mode === 'light'
-                                ? theme.palette.grey[200]
-                                : theme.palette.grey[700],
-                                }}
-                                />
-                                <CardContent>
-                                    <Box
-                                    sx={{
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'baseline',
-                                    mb: 2,
-                                    }}
-                                    >
-                                        <Typography variant="h4" color="text.primary">
-                                        {plan.currency}
-                                        </Typography>
-                                        <Typography component="h2" variant="h3" color="text.primary">
-                                        {plan.price}
-                                        </Typography>
-                                        <Typography variant="h6" color="text.secondary">
-                                        /{plan.frequency}
-                                        </Typography>
-                                    </Box>
-                                    <ul style={{listStyleType: 'none', paddingLeft: '0px'}}>
-                                    {plan.description.map((line) => (
-                                        <Typography
-                                        component="li"
-                                        variant="subtitle1"
-                                        align="center"
-                                        key={line}
-                                        >
-                                            {line}
-                                        </Typography>
-                                    ))}
-                                    </ul>
-                                </CardContent>
-                                <CardActions>
-                                    <Button fullWidth variant={plan.popular?"contained":"outlined"}>
-                                    Continue
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
-                    </Grid>
-                </Box>
+                {priceId === "" && 
+                    <PriceTable setPriceId={setPriceId} plans={plans} />
+                }
             </Paper>
         </Container>
     )
