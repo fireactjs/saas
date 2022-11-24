57a38483acb50abb20085b56eb3f64fcb02dceb8
Chaoming Li <chaoming@users.noreply.github.com>
2022-11-24 18:24:35 +1100

    added design to ListSubscription

--- src/lib/components/ListSubscriptions.js
+++ src/lib/components/ListSubscriptions.js
@@ -1,7 +1,14 @@
+import { SetPageTitle } from "@fireactjs/core";
+import { Container, Paper } from "@mui/material";
 import React from "react";
 
 export const ListSubscriptions = () => {
     return (
-        <></>
+        <Container>
+            <SetPageTitle title="My Subscriptions" />
+            <Paper>
+                <h2>My Subscriptions</h2>
+            </Paper>
+        </Container>
     )
 }
