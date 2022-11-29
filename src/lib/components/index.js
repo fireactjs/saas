70ee31432412aa10f125860b8118be54ec5974dd
Chaoming Li <chaoming@users.noreply.github.com>
2022-11-29 18:59:51 +1100

    added checkPermission

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -2,8 +2,10 @@ import { CreateSubscription } from "./CreateSubscription";
 import { ListSubscriptions } from "./ListSubscriptions";
 import pathnames from './pathnames.json';
 import { SubscriptionLoader } from "./SubscriptionLoader";
+import { checkPermission } from "./utilities";
 
 export {
+    checkPermission,
     CreateSubscription,
     ListSubscriptions,
     pathnames,
