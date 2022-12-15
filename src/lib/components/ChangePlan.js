cc7532b6ea9769be9a19608d19299029d4e24ebe
Chaoming Li <email@chaoming.li>
2022-12-15 23:10:37 +1100

    added success process

--- src/lib/components/ChangePlan.js
+++ src/lib/components/ChangePlan.js
@@ -5,11 +5,14 @@ import { PricingPlans } from "./PricingPlans";
 import { SubscriptionContext } from "./SubscriptionContext";
 import "firebase/compat/functions";
 import { PaymentMethodForm } from "./PaymentMethodForm";
+import { NavLink } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 export const ChangePlan = () => {
 
     const { subscription, setSubscription } = useContext(SubscriptionContext);
     const { config } = useContext(FireactContext);
+    const auth = getAuth();
 
     const { firebaseApp } = useContext(AuthContext);
     const CloudFunctions = firebaseApp.functions();
@@ -18,6 +21,7 @@ export const ChangePlan = () => {
     const [ error, setError ] = useState(null);
     const [ showPaymentMethod, setShowPaymentMethod ] = useState(false);
     const [ selectedPlan, setSelectedPlan ] = useState(null);
+    const [ success, setSuccess ] = useState(false);
 
     const selectPlan = (plan) => {
         setProcessing(true);
@@ -30,6 +34,7 @@ export const ChangePlan = () => {
                 priceId: plan.priceId,
                 subscriptionId: subscription.id
             }).then(() => {
+                setSuccess(true);
                 setProcessing(false);
             }).catch(err => {
                 setError(err.message);
@@ -52,6 +57,15 @@ export const ChangePlan = () => {
             priceId: selectedPlan.priceId,
             subscriptionId: subscription.id
         }).then(() => {
+            const pmRef = firebaseApp.firestore().doc('users/'+auth.currentUser.uid+'/paymentMethods/'+paymentMethod.id);
+            return pmRef.set({
+                type: paymentMethod.type,
+                cardBrand: paymentMethod.card.brand,
+                cardExpMonth: paymentMethod.card.exp_month,
+                cardExpYear: paymentMethod.card.exp_year,
+                cardLast4: paymentMethod.card.last4
+            },{merge:true});
+        }).then(() => {
             setSubscription(prevState => ({
                 ...prevState,
                 plan: selectedPlan.title, // title of the plan
@@ -60,7 +74,8 @@ export const ChangePlan = () => {
                 price: selectedPlan.price,
                 currency: selectedPlan.currency,
                 paymentMethod: paymentMethod.id
-            }))
+            }));
+            setSuccess(true);
             setProcessing(false);
         }).catch(err => {
             setError(err.message);
@@ -71,46 +86,49 @@ export const ChangePlan = () => {
     return (
         <Container maxWidth="lx">
             <SetPageTitle title={"Change Plan"+(subscription.name!==""?(" - "+subscription.name):"")} />
-            <Paper>
-                <Box p={5}>
-                    {showPaymentMethod?(
-                        <Stack spacing={3}>
-                            <Typography
-                            component="h1"
-                            variant="h3"
-                            align="center"
-                            color="text.primary"
-                            gutterBottom
-                            >
-                            Setup Payment Method
-                            </Typography>
-                            {error !== null && 
-                                <Alert severity="error">{error}</Alert>
-                            }
-                            <PaymentMethodForm buttonText={"Submit"} setPaymentMethod={submitPlan} disabled={processing} />               
-                        </Stack>
-                    ):(
-                        <Stack spacing={3}>
-                            <Typography
-                            component="h1"
-                            variant="h3"
-                            align="center"
-                            color="text.primary"
-                            gutterBottom
-                            >
-                            Choose a Plan
-                            </Typography>
-                            {error !== null && 
-                                <Alert severity="error">{error}</Alert>
-                            }
-                            <div>
-                                <PricingPlans selectedPriceId={subscription.stripePriceId} selectPlan={selectPlan} disabled={processing} />
-                            </div>
-                        </Stack>
-                    )}
-                </Box>
-                
-            </Paper>
+            {success?(
+                <Alert severity="success">Your subscription plan has been changed. Please go back to <NavLink to={config.pathnames.ListInvoices.replace(":subscriptionId", subscription.id)}>Billing</NavLink>.</Alert>
+            ):(
+                <Paper>
+                    <Box p={5}>
+                        {showPaymentMethod?(
+                            <Stack spacing={3}>
+                                <Typography
+                                component="h1"
+                                variant="h3"
+                                align="center"
+                                color="text.primary"
+                                gutterBottom
+                                >
+                                Setup Payment Method
+                                </Typography>
+                                {error !== null && 
+                                    <Alert severity="error">{error}</Alert>
+                                }
+                                <PaymentMethodForm buttonText={"Submit"} setPaymentMethod={submitPlan} disabled={processing} />               
+                            </Stack>
+                        ):(
+                            <Stack spacing={3}>
+                                <Typography
+                                component="h1"
+                                variant="h3"
+                                align="center"
+                                color="text.primary"
+                                gutterBottom
+                                >
+                                Choose a Plan
+                                </Typography>
+                                {error !== null && 
+                                    <Alert severity="error">{error}</Alert>
+                                }
+                                <div>
+                                    <PricingPlans selectedPriceId={subscription.stripePriceId} selectPlan={selectPlan} disabled={processing} />
+                                </div>
+                            </Stack>
+                        )}
+                    </Box>
+                </Paper>
+            )}
             
         </Container>
     )
